51907d1fbfd30194e4e75c7e38084cc7c91afca8
kaminto1999 <kaminto1999@gmail.com>
2022-10-05 13:40:47 +0800

    update

--- isLongPressedName.s
+++ isLongPressedName.s
@@ -1,42 +1,37 @@
-.section .text
-.align 2
-.globl main
-.global addr_name
-.global addr_type
-addr_name: .string "a"
-addr_type: .string "a"
+.data
+addr_name:  .byte 0x97, 0x6C, 0x65, 0x78, 0x00
+addr_typed: .byte 0x97, 0x6C, 0x65, 0x65, 0x78, 0x78, 0x00
+str1:       .string "a0 is "
 
+.text
 main:
-    addi  sp, sp, -4
-    sw    ra, 0(sp)
     la    t0, addr_name #Load "name" 1st address
-    la    t1, addr_type #Load "type" 1st address
+    la    t1, addr_typed #Load "typed" 1st address
     lb    t2, 0(t0)     #Load 1st character of name
-    lb    t3, 0(t1)     #Load 1st character of type
+    lb    t3, 0(t1)     #Load 1st character of typed
     bne   t2, t3, Fail  #1st character must the same
 
-Loop:
+Loop: #1st while loop
     addi  t0, t0, 1     #i++
     addi  t1, t1, 1     #j++
     lb    t2, 0(t0)     #Load name[i]
-    lb    t3, 0(t1)     #Load type[j]
-    bne   t2, t3, checkLongPressed   #If name[i]!=type[j], check long pressed
-    beq   t2, x0, checkLastCharacter #If name[i]= NULL, check type[j]
+    lb    t3, 0(t1)     #Load typed[j]
+If:
+    bne   t2, t3, Else   #If name[i]!=typed[j], check long pressed
+    beq   t2, x0, checkLastCharacter #If name[i]= NULL, check typed[j]
     j     Loop
-
-checkLongPressed:
+Else:
     addi  t0, t0, -1    # Previous character address
     lb    t2, 0(t0)     # Load name[i-1]
     bne   t2, t3,Fail   # If typed[j] != name[i-1] then Fail
     addi  t1, t1, 1     # j++
     j     Loop          
-
-checkLastCharacter:
-    beq   t3, x0, Pass  # If type[j] = NULL, then passed
+checkLastCharacter: #2nd while loop
+    beq   t3, x0, Pass  # If typed[j] = NULL, then passed
     addi  t0, t0, -1    # i--
     lb    t2, 0(t0)     # Load last character of name
     bne   t2, t3,Fail   # If typed[j] != name[i-1] then Fail
-    j     checkLongPressed
+    j     Else
 
 Pass:
     li    a0, 1         # Return a0 =1
@@ -47,4 +42,16 @@ Fail:
 End:
     sw    ra, 0(sp)
     addi  sp, sp, -4
-    jr ra
+    jal ra, printResult
+    li a7, 10
+    ecall
+printResult:
+        mv t0, a0
+        mv t1, a1
+        la a0, str1
+        li a7, 4
+        ecall
+        mv a0, t0
+        li a7, 1
+        ecall
+        ret
